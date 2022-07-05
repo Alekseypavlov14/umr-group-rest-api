5ee592f1f0cff7d00f42a293930eb3b6d41c3ea3
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-05 10:33:30 +0300

    make password field not unique

--- models/Admin.model.js
+++ models/Admin.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const adminSchema = new Schema({
   login: { type: String, unique: true, required: true },
-  password: { type: String, unique: true, required: true }
+  password: { type: String, required: true }
 })
 
 const Admin = model('admin', adminSchema)
